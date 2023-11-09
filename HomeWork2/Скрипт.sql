091a96fea8d45f2b31507b0469be6ee8bc5a9a16
Marat <108996479+Marat1988@users.noreply.github.com>
2023-11-09 16:52:59 +0200

    Update Скрипт.sql

--- HomeWork2/Скрипт.sql
+++ HomeWork2/Скрипт.sql
@@ -1,16 +1,5 @@
 ﻿CREATE DATABASE IF NOT EXISTS TestDb;
 USE TestDb;
-CREATE TABLE Customers
-(
-    Id INT AUTO_INCREMENT,
-    Age INT,
-    FirstName VARCHAR(20) NOT NULL,
-    LastName VARCHAR(20) NOT NULL,
-    Email VARCHAR(30),
-    Phone VARCHAR(20) NOT NULL,
-    CONSTRAINT `customers_pk` PRIMARY KEY(Id),
-    CONSTRAINT `customer_phone_uq` UNIQUE(Phone)
-);
 
 CREATE TABLE Users
 (
@@ -18,7 +7,7 @@ CREATE TABLE Users
     fullName VARCHAR(50) NOT NULL CHECK (fullname!=''),
     login VARCHAR(20) NOT NULL CHECK (login!=''),
     password VARCHAR(20) NOT NULL CHECK (password!=''),
-    email VARCHAR(20) NOT NULL CHECK (email!=''),
+    email VARCHAR(50) NOT NULL CHECK (email!=''),
     CONSTRAINT users_id_pk PRIMARY KEY(Id),
     CONSTRAINT users_login_uq UNIQUE(login),
     CONSTRAINT users_email_uq UNIQUE(email)
